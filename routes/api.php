b678ba7d541bc8b7ac6332edf25ec9ff1b30a777
Alina Abdul <alinka140901@gmail.com>
2023-09-16 10:09:39 +0300

    Инициализирован проект, созданы роуты

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/employee', []);
+
+Route::post('/employee/hours', []);
+
+Route::get('/salaries', []);
+
+Route::delete('/salaries', []);
